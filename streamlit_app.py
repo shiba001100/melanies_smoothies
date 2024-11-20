4d333f294579669347cfb8006b697b9dc1368329
shiba001100 <shibata.shotaro.4864@members.co.jp>
2024-11-20 10:17:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         
             st.subheader(fruit_chosen + ' Nutrition Information')
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-            df = open(smoothiefroot_response)
+            df = json.load(smoothiefroot_response)
             print(df)
             st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
             
